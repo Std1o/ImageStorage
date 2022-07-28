5751400665aadc2311674c3f4fa9a7361bf8fbbe
Std1o <37378410+Std1o@users.noreply.github.com>
2022-07-28 23:19:13 +0300

    create_upload_file

--- src/image_storage/api/images.py
+++ src/image_storage/api/images.py
@@ -1,9 +1,22 @@
-
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from fastapi.responses import FileResponse
+import shutil
+from pathlib import Path
+
 
 router = APIRouter(prefix='/images')
 
 @router.get('/{image_name}')
 def get_image(image_name: str):
     return FileResponse(f"../../images/{image_name}.png")
+
+@router.post("/uploadfile/")
+async def create_upload_file(upload_file: UploadFile):
+    destination = Path(f"../../images/{upload_file.filename}")
+    print(destination.absolute())
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+    return {"filename": upload_file.filename}

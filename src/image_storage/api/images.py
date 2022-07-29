3a684b36eb9775d84fe21e5fe116d38402356f8c
Std1o <37378410+Std1o@users.noreply.github.com>
2022-07-29 22:43:19 +0300

    fix uploading

--- src/image_storage/api/images.py
+++ src/image_storage/api/images.py
@@ -2,13 +2,15 @@ from fastapi import APIRouter, UploadFile
 from fastapi.responses import FileResponse
 import shutil
 from pathlib import Path
+import os
+import re
 
 
 router = APIRouter(prefix='/images')
 
 @router.get('/{image_name}')
 def get_image(image_name: str):
-    return FileResponse(f"../../images/{image_name}.png")
+    return FileResponse(f"../../images/{image_name}")
 
 @router.post("/uploadfile/")
 async def create_upload_file(upload_file: UploadFile):
@@ -19,4 +21,7 @@ async def create_upload_file(upload_file: UploadFile):
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
         upload_file.file.close()
-    return {"filename": upload_file.filename}
+    ip_config = os.popen('ifconfig | grep "inet " | grep -v 127.0.0.1').readline().replace(" ", "")
+    ip = re.search('inet(.*)netmask', ip_config)
+    print(ip.group(1))
+    return f"http://{ip.group(1)}/images/{upload_file.filename}"

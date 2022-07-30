ddac6541f43388d674c311e58a2606c3eb83872d
Std1o <37378410+Std1o@users.noreply.github.com>
2022-07-31 00:36:38 +0300

    encode utf-8 fix

--- src/image_storage/api/images.py
+++ src/image_storage/api/images.py
@@ -10,14 +10,12 @@ router = APIRouter(prefix='/images')
 
 @router.get('/{image_name}')
 def get_image(image_name: str):
-    return FileResponse(f"../../images/{image_name}")
+    return FileResponse(f"../../images/{image_name}".encode('utf-8').decode('utf-8'))
 
 @router.post("/uploadfile/")
 async def create_upload_file(upload_file: UploadFile):
-    destination = Path(f"../../images/{upload_file.filename}")
-    print(destination.absolute())
     try:
-        with destination.open("wb") as buffer:
+        with open(f"../../images/{upload_file.filename}".encode('utf-8'), "wb") as buffer:
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
         upload_file.file.close()
